b91afd642699d0b210ebf35dee1b7c49a63fb4bd
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-01-12 17:30:11 +0530

    modified:   .gdb_history 	new file:   solved_prob1.c

---
+++ cInDepth/7_Recursion/solved_prob1.c
@@ -0,0 +1,14 @@
+/* How many times this get function being called */
+#include <stdio.h>
+void get(int number) {
+	if (number < 1)
+		return;
+	get(number-1);
+	get(number-3);
+	printf("%d ", number);
+}
+
+int main() {
+	get(6);
+	return 0;
+}
