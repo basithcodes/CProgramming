309c7bc9985b47d6afafda45506fbd26517c2e4a
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-01-14 12:51:49 +0530

    new file:   solved_prob2.c 	new file:   solved_prob3.c 	new file:   solved_prob4.c

---
+++ cInDepth/7_Recursion/solved_prob4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fun(int n) {
+	int x = 1, k;
+	if (n == 1) return x;
+	for (k = 1; k < n; ++k) 
+		x = x + fun(k) * fun(n - k);
+	return x;
+}
+
+int main() {
+	int n;
+	while (1) {
+		printf("Enter n value: ");
+		scanf("%d", &n);
+		fun(n);
+		printf("\n");
+	}
+
+}
