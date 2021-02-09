22ddf11f4365862876b45cb97b9aa0c9a4059314
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-02-09 20:23:13 +0530

    modified:   headFirstC/geo2json 	new file:   headFirstC/jsonfile.txt 	modified:   vivenEmbeddedAcademy/.gdb_history 	modified:   vivenEmbeddedAcademy/a.out 	modified:   vivenEmbeddedAcademy/test.c

--- vivenEmbeddedAcademy/test.c
+++ vivenEmbeddedAcademy/test.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main() {
-	char x = '97';
-	printf("%d - %c\n", x, x);
+	int a = 5,b = 6;
+	printf("a = %d, b = %d\n", a, b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a = %d, b = %d\n", a, b);
 }
