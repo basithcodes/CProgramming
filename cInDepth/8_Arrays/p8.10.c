8bb63be564d822e459aa1e79ac30512636c872a4
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-02-16 20:25:43 +0530

    modified:   a.out 	new file:   p8.10.c 	modified:   p8.8.c 	new file:   p8.9.c

---
+++ cInDepth/8_Arrays/p8.10.c
@@ -0,0 +1,23 @@
+/* Program to input and display a matrix */
+#include <stdio.h>
+#define ROWS 3
+#define COLUMNS 4
+
+int main() {
+	int i, j;
+	int arr[ROWS][COLUMNS];
+
+	/* Scan the elements */
+	for (i = 0; i < ROWS; i++)
+		for (j = 0; j < COLUMNS; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	/* Print the elements of arrays */
+	for (i = 0; i < ROWS; i++) {
+		for (j = 0; j < COLUMNS; j++) {
+			printf("%d\t", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+}
