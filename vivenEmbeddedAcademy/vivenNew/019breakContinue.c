c21fd344df5c5a074e0f74e52d9bc7c4d08e2958
Abdul Basith <abdulbasith.eee@hotmail.com>
2021-04-10 13:04:49 +0530

    modified:   vivenEmbeddedAcademy/vivenNew/019breakContinue.c 	modified:   vivenEmbeddedAcademy/vivenNew/a.out

--- vivenEmbeddedAcademy/vivenNew/019breakContinue.c
+++ vivenEmbeddedAcademy/vivenNew/019breakContinue.c
@@ -2,7 +2,21 @@
 
 int main(void)
 {
-	printf("Hello");
+	int j, max, min, count = 0;
+	printf("Enter max and min values: ");
+	scanf("%d %d", &max, &min);
+	while (min <= max) {
+		for (j = 2, count = 0; j <= min; ++j) {
+			if(min%j == 0) {
+				count++;
+				if (count > 1)
+					break;
+			}
+		}
+		if (count == 1)
+			printf("%d ", min);
+		min++;
+	}
+	printf("\n");
 	return 0;
 }
-
